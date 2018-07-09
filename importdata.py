0a02fb3edacf762ffa0fb1576fccef525d1f6e6b
ycharm <yashasharma@gmail.com>
2018-07-08 20:34:48 -0400

    Pubmed Data

--- importdata.py
+++ importdata.py
@@ -44,6 +44,64 @@ for page in range(1, numPages + 1):
         df_temp=pd.DataFrame.from_dict(json_normalize(data),orient='columns')
         df = pd.concat([df,df_temp])
 
+#save file
 df.to_pickle('Healthgradesdoctorlist.pkl')
 
+#add imports from Pubmed and Scopus
+#ccreate a dictionary containing pubmed IDs of all articles published by doctors
+pub_dict = {}
+#read email address
+file_object = open('email.key', 'r')
+email = file_object.read()
+file_object.close()
+Entrez.email = file_object
+count = 0
+for name in df.head(1)['fullname']:
+    if not name in publist.keys():
+        searchterm = '(cancer[MeSH Terms]) AND ' + name + '[Author] '
+        handle = Entrez.esearch(db="Pubmed", retmax=500, term= searchterm, idtype="acc")
+        record = Entrez.read(handle)
+        handle.close()
+        pub_dict[name] = record['IdList']
+        time.sleep(0.4)
+        count+=1
 #
+
+#make a big list of all pubmed IDs (PMID)
+#get article list and combine into big list
+uidlist_master = []
+for name in pub_dict:
+    uidlist_master += pub_dict[name]
+
+
+#Get full abstracts for all pubmed IDs (PMID) in uidlist_master
+handle = Entrez.efetch(db='pubmed', id=uidlist_master, retmode='xml', rettype='abstract')
+record = Entrez.read(handle)
+handle.close()
+
+
+extract articletitle, journal title, year and abstract
+#Use dictionaries to be very careful
+articletitle = {}
+journaltitle = {}
+journalyear = {}
+abstract = {}
+
+for article in record['PubmedArticle']:
+    ID = (article['MedlineCitation']['PMID'])
+    articletitle[ID] = article['MedlineCitation']['Article']['ArticleTitle']
+    journaltitle[ID] = (article['MedlineCitation']['Article']['Journal']['Title'])
+    if 'Year' in article['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']:
+        journalyear[ID] = (article['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year'])
+    if 'Abstract' in article['MedlineCitation']['Article']:
+        abstract[ID] = (article['MedlineCitation']['Article']['Abstract']['AbstractText'][0])
+
+#create a dataframe of with four columns for each publication ID
+df_temp1 = pd.DataFrame.from_dict(articletitle,orient = 'index', columns = ['ArticleTitle'])
+df_temp2 = pd.DataFrame.from_dict(journaltitle,orient = 'index', columns = ['JournalTitle'])
+df_temp3 = pd.DataFrame.from_dict(journalyear,orient = 'index', columns = ['JournalYear'])
+df_temp4 = pd.DataFrame.from_dict(abstract,orient = 'index', columns = ['Abstract'])
+pub_df = pd.concat([df_temp1,df_temp2,df_temp3,df_temp4], axis=1, sort=False)
+
+#save as a .pkl file
+pub_df.to_pickle('ArticleDetails.pkl')
